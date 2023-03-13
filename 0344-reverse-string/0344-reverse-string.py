dfd6b6dee5399b3f46ce25661303f2f78ca159ab
Miran Lee <ranz_@naver.com>
2023-03-13 20:11:38 +0900

    Time: 211 ms (30.38%), Space: 18.4 MB (79.62%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,8 +4,4 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         for i in range(len(s)//2):
-            tmp = s[i]
-            s[i] = s[len(s)-1-i]
-            s[len(s)-1-i] = tmp
-            print(s[i], s[len(s)-1-i])
-            
+            s[i], s[len(s)-1-i] = s[len(s)-1-i], s[i]
