692e9e401215a1bc37782c496884df1fdc3971f8
Miran Lee <ranz_@naver.com>
2023-06-12 20:09:04 +0900

    Time: 665 ms (70.48%), Space: 45.7 MB (32.48%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -2,6 +2,11 @@ import bisect
 
 class SnapshotArray:
     
+    # naive하게 풀면 풀리는 문제인데, 공간 효율성 limit 때문에 다른 방법 찾아야 했음..
+    # 변경된 인자들만 저장하면 되겠다! 까지는 생각해서 풀었는데,
+    # 만약 변경 시점에 snap이 없었을 때 그냥 for문 돌려서 했더니 time out 에러 남..
+    # bisect 라이브러리 사용해서 binary search를 하면 풀림...ㅎ
+    
     array_history = []
     snap_id = 0
 
