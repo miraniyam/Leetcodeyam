ca39fc862bdb30dc2afd278f0b4926d5eb1ffdc3
Miran Lee <ranz_@naver.com>
2023-06-29 12:20:04 +0900

    Time: 47 ms (71.33%), Space: 16.3 MB (37.88%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,23 @@
+"""
+k row
+node[i] = node[k]
+"""
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        memo = []
+        for i in range(numRows):
+            memo.append([1 for _ in range(0,i+1)])
+        
+        for i in range(0, len(memo)):
+            for j in range(0, len(memo[i])):
+                if i == 0 or j == 0:
+                    continue
+                if j > len(memo[i-1]) - 1:
+                    continue
+                memo[i][j] = memo[i-1][j-1]+memo[i-1][j]
+                
+        return memo
+                
+        
+        
