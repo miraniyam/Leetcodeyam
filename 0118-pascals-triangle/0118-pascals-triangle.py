ef9f19b4cda0892c4677ad15d5148cbef6d8e032
Miran Lee <ranz_@naver.com>
2023-06-29 12:21:54 +0900

    Time: 47 ms (71.33%), Space: 16.3 MB (78.96%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -9,12 +9,12 @@ class Solution:
         for i in range(numRows):
             memo.append([1 for _ in range(0,i+1)])
         
-        for i in range(0, len(memo)):
-            for j in range(0, len(memo[i])):
-                if i == 0 or j == 0:
-                    continue
-                if j > len(memo[i-1]) - 1:
-                    continue
+        for i in range(1, len(memo)):
+            for j in range(1, len(memo[i])-1):
+                # if i == 0 or j == 0:
+                #     continue
+                # if j > len(memo[i-1]) - 1:
+                #     continue
                 memo[i][j] = memo[i-1][j-1]+memo[i-1][j]
                 
         return memo
